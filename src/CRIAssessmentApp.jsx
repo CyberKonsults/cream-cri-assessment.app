5ea1b747eb6b636ec81f25691e29f701467a321a
iamtunji.personal <tunjitaiwo@cyberkonsults.com>
2025-06-15 15:29:25 -0400

    Update CRIAssessmentApp.jsx

--- src/CRIAssessmentApp.jsx
+++ src/CRIAssessmentApp.jsx
@@ -8,8 +8,11 @@ import jsPDF from "jspdf";
 import autoTable from "jspdf-autotable";
 import { createClient } from "@supabase/supabase-js";
 
-const supabase = createClient("https://iiqtmkqdfphslwbettxx.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlpcXRta3FkZnBoc2x3YmV0dHh4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk5MzQ0NDMsImV4cCI6MjA2NTUxMDQ0M30.Glc6eUxfQqEPw4yPtRXYIc1L7TqUyPYydbUP4cCbz74");
-//const supabase = createClient("https://iiqtmkqdfphslwbettxx.supabase.co", "public-anon-key");
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+);
+
 function mockAIScore(response) {
   if (!response) return { score: 0, confidence: "Low" };
   if (response.includes("least privilege") || response.includes("inventory")) return { score: 3, confidence: "High" };
